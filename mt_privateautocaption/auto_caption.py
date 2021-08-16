73c27705d26ee9275f6ba38e88c00725de37206e
BOTS Infinity <87800728+Doc-Dx@users.noreply.github.com>
2021-08-16 10:49:34 +0530

    Update auto_caption.py

--- mt_privateautocaption/auto_caption.py
+++ mt_privateautocaption/auto_caption.py
@@ -5,7 +5,7 @@
 import os
 from config import Config
 from pyrogram import Client, filters 
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.types import InlineKeyboardMarkup, Message
 from pyrogram.errors import FloodWait
 
 CAPTION_TEXT=Config.CAPTION
@@ -16,12 +16,7 @@ TXT=""" [infinity](https://t.me/infinityclk) """
 @Client.on_message(filters.media & filters.channel)
 async def caption(client, message: Message):
     kopp, _ = get_file_id(message)
-    await message.edit(f"<code>{kopp.file_name}</code>\n\n{TXT}",
-          reply_markup=InlineKeyboardMarkup(
-              [[
-              InlineKeyboardButton(f"{BUTTON_TEXT}", url=f"{URL_LINK}")
-              ]]
-        ))
+    await message.edit(f"<code>{kopp.file_name}</code>\n\n{TXT}"
 
 def get_file_id(msg: Message):
     if msg.media:
