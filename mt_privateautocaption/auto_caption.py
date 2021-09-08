1419b4ca89665ea02026bb55893419c13ef472e3
BOTS Infinity <87800728+Doc-Dx@users.noreply.github.com>
2021-09-08 11:59:24 +0530

    Update auto_caption.py

--- mt_privateautocaption/auto_caption.py
+++ mt_privateautocaption/auto_caption.py
@@ -8,12 +8,13 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 from pyrogram.errors import FloodWait
 
-TXT=""" **[ʙᴏᴛꜱ](https://t.me/bots_infinity)| [ᴍᴏᴠɪᴇꜱ](https://t.me/infinityclk) | [ꜱᴇʀɪᴇꜱ](https://t.me/infinityclk)** """
+TXT=""" **▫️ [ʙᴏᴛꜱ](https://t.me/bots_infinity) | [ᴍᴏᴠɪᴇꜱ](https://t.me/infinityclk) | [ꜱᴇʀɪᴇꜱ](https://t.me/infinitylk) ▫️** """
+TVA="""[@InfinityLK]."""
 
 @Client.on_message(filters.media & filters.channel)
 async def caption(client, message: Message):
     kopp, _ = get_file_id(message)
-    await message.edit(f"<code>{kopp.file_name}</code>\n\n{TXT}")
+    await message.edit(f"<code>{TVA}</code><code>{kopp.file_name}</code>\n\n{TXT}")
 
 def get_file_id(msg: Message):
     if msg.media:
