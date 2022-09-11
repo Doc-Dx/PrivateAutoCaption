b4a34f74113f3ca7c438fc43aef895c428082219
BOTS Infinity <87800728+Doc-Dx@users.noreply.github.com>
2022-09-11 14:45:08 +0530

    Update auto_caption.py

--- mt_privateautocaption/auto_caption.py
+++ mt_privateautocaption/auto_caption.py
@@ -11,13 +11,13 @@ from pyrogram.errors import FloodWait
 CAPTION_TEXT=Config.CAPTION
 BUTTON_TEXT=Config.BUTTON_TEXT
 URL_LINK=Config.URL_LINK
-TXT=""" [infinity](https://t.me/infinityclk) """
-TVT=""" [@InfinityLK]. """
+TXT="""• @infinityLK | @infinityCLK •"""
+TVT="""➥ File Name :"""
 
 @Client.on_message(filters.media & filters.channel)
 async def caption(client, message: Message):
     kopp, _ = get_file_id(message)
-    await message.edit(f"<code>{TVT}{kopp.file_name}</code>\n\n{TXT}",
+    await message.edit(f"<code>{TVT} {kopp.file_name}</code>\n\n{TXT}",
           reply_markup=InlineKeyboardMarkup(
               [[
               InlineKeyboardButton(f"{BUTTON_TEXT}", url=f"{URL_LINK}")
